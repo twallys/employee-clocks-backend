ba7c6207e5b903cf0caeab766b62454bede68f73
Talles Reis <talles.eclesia@gmail.com>
2020-12-13 18:27:03 -0300

    CORS

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 
 // Enable CORS
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header(
         'Access-Control-Allow-Headers',
         'x-requested-with, content-type',
@@ -24,6 +24,7 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Max-Age', '1000000000');
 
+    // intercept OPTIONS method if ('OPTIONS' == req.method) { res.send(200); } else { next(); } });
     next();
 });
 

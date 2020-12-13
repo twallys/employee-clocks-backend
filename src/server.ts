4b2e4c98835d261e53af05c69653806da1202120
Talles Reis <talles.eclesia@gmail.com>
2020-12-13 16:12:03 -0300

    CORS

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, NextFunction } from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 import 'express-async-errors';
 import routes from './routes';
 import uploadConfig from './config/upload';
@@ -9,9 +9,18 @@ import './database';
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
+
+// Enable CORS
+app.use(function( req, res, next ) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "x-requested-with, content-type");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Max-Age", "1000000000");
+    // intercept OPTIONS method if ('OPTIONS' == req.method) { res.send(200); } else { next(); } });
 
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);

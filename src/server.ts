b99b4030a72cf5e9b6a9b6a324eff943f1348c68
Talles Reis <talles.eclesia@gmail.com>
2020-12-13 16:03:10 -0300

    CORS

--- src/server.ts
+++ src/server.ts
@@ -37,6 +37,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     });
 });
 
-app.listen(80, () => {
+app.listen(3333, () => {
     console.log('🚀 Server started on port 3333');
 });
